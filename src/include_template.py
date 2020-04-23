8ae4423c4b610ced7f2c885d3fa2ac2ac5c5183b
aaronhma <aaronhma@gmail.com>
2020-04-22 20:34:55 -0700

    Create include_template.py

---
+++ src/include_template.py
@@ -0,0 +1,6 @@
+from external.common import log_states
+from perception import camera
+from planning import plan_route
+from trajectory import trajectory_plot
+
+# TODO(aaronhma): Should we provide API declarations?
